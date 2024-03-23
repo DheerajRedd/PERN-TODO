8b8f97e6b52138d58b238d3b7c0a1c9b74b51037
dheerajaayats <dheeraj.reddy@aayats.com>
2024-03-23 12:17:00 -0400

    Postgres and express connection

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React, { Fragment } from "react";
+import Input from "./components/Input";
+import List from "./components/List";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <Input />
+        <List />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/List.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+const List = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:3005/todos");
+      const jsonData = await response.json();
+      setTodos(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <Fragment>
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default List;
